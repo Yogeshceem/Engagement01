3dfcc6c223ab1f58fdf51327a97b08c21af1ddb8
Yogesh <156497745+Yogeshceem@users.noreply.github.com>
2024-01-26 19:03:01 -0600

    Update S5732178Eng01.java

--- S5732178Eng01/src/main/java/edu/nwmissouri/spring24/cs44542/sec01/eng01/s5732178eng01/S5732178Eng01.java
+++ S5732178Eng01/src/main/java/edu/nwmissouri/spring24/cs44542/sec01/eng01/s5732178eng01/S5732178Eng01.java
@@ -5,9 +5,15 @@
 package edu.nwmissouri.spring24.cs44542.sec01.eng01.s5732178eng01;
 
 /**
- *
- * @author S572178
- */
+* Class: 44542-01 Object-Oriented Programming
+*
+* @author Yogesh Ceemarla
+* Description: Making sure everything works
+* Due: 01/26/2024
+* I pledge that I have completed the programming assignment independently.
+* I have not copied the code from a student, internet, or any other source.
+* I will not share my source code with anyone under any circumstances.
+*/
 import java.util.*;
 
 public class S5732178Eng01 {
